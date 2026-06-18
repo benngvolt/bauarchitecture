2dfe6dd8f55a519fd652fa1c6522cd6e6c4e67f7
Benjamin Gibert <contact@benjamingibert.com>
2026-06-18 11:15:06 +0200

    2606181115

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,4 @@
 //export const API_URL = 'http://localhost:3000';
 
-export const API_URL = 'https://fiery-plate-429713-j6.oa.r.appspot.com'
+// export const API_URL = 'https://fiery-plate-429713-j6.oa.r.appspot.com'
+export const API_URL ='https://bauapi.bengibert.com'
